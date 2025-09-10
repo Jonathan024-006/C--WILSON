6ec1b2e25cc6e6376c02c49881df51345af0e48f
Florez <jonathanflorez006@gmail.com>
2025-09-10 09:15:37 -0500

    commit ejercicio completo 2

--- Clase1/obj/Debug/net9.0/C#.AssemblyInfo.cs
+++ Clase1/obj/Debug/net9.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a4175c9e10b1d8eb7bd26bc1b0e1fb09e0a6dbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6081595768ca991d6db78e53f42d89ca80ca9d89")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
